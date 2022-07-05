e45f5813428eb9f9916fe2a95afa74e5e13f4d01
ethaneng <90435413+ethaneng@users.noreply.github.com>
2022-07-05 13:00:09 +0800

    Edited server/database/db.js to use .env details instead of holding sensitive info in the file

--- server/database/db.js
+++ server/database/db.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool
 
 const pool = new Pool({
-    user: "postgres",
-    password: "F@zzieb75",
-    host: "localhost",
-    port: 5432,
-    database: "mwa_proposals"
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME
 })
 
 module.exports = pool
